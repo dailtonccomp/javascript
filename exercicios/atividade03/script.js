4fd38500e378dba9855a2b2aac24311f875e35e9
dailtonccomp <112089713+dailtonccomp@users.noreply.github.com>
2022-09-23 17:55:24 -0300

    por hoje ta ok

--- exercicios/atividade03/script.js
+++ exercicios/atividade03/script.js
@@ -1,7 +1,6 @@
 function contar(){    
     var ini = document.getElementById('inicio')
     ini= Number(ini.value)
-
     var fim = document.getElementById('fim')
     fim = Number(fim.value)
     var passo = document.getElementById('passo')
@@ -9,24 +8,75 @@ function contar(){
     var res = document.querySelector('div#res')    
     if(ini == ' ' || fim == ' ') {
         res.innerHTML = 'Nenhuma caixa pode ficar em branco!'
-    } else if(ini > fim) {
-        res.innerHTML = 'O valor de fim não pode ser menor que o inicio!'
-    }else {
+    } else {
         if(passo == 0) {
             passo = 1
             window.alert('Passo inválido! Considerando Passo igual a 1')
-        }
-        let contagem = [] 
+        }  
+        res.innerHTML = 'Contando: '      
         var calc = ini
-        res.innerHTML = `inicio: ${ini}, fim: ${fim} e passo: ${passo}`
-            while(calc <= fim) {             
-            contagem.push(calc)
-            calc = calc + passo
+        var simbolo = ''
+        if(ini < fim) {
+            while(calc <= fim) {
+                if(calc < fim){
+                    simbolo = '&#x1F449'
+                }else {
+                    simbolo = '&#x1F3C1'
+                }
+                res.innerHTML += ` ${calc} ${simbolo}`                
+                calc = calc + passo
             }
-        //     // var qtde = contagem.length
-        //res.innerHTML = `${qtde} com resultado ${contagem}`        
-        res.innerHTML = `${contagem[0]} &#x1F449 `
-        //res.innerHTML = contagem
+        }else {
+            while(calc >= fim) { 
+                if(calc > fim){
+                    simbolo = '&#x1F449' // pode ser representado também por \u{1F449}
+                }else {
+                    simbolo = '&#x1F3C1'
+                }
+                res.innerHTML += ` ${calc} ${simbolo}`
+                calc = calc - passo
+            }
+        }        
+    }   
+}
+
+/*
+    Código guanabara
+    function contar{
+        let ini = document.getElementById('txti')
+        let fim = document.getElementById('txtf')
+        let passo = document.getElementById('txtp')
+        let res = document.getElementById('res')
+
+        if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0)
+        {
+            res.innerHTML = 'Impossível contar!'
+        } else {
+            res.innerHTML = 'Contando: <br>'
+            let i = Number(ini.value)
+            let f = Number(fim.value)
+            let p = Number(passo.value)
+            if( p <= 0) {
+                window.alert('Passo inválido! Considerando PASSO 1')
+                p=1
+            }
+            if(i < f) {
+                //contagem crescente
+                for(let c = i; c <= f; c += p) {
+                    res.innerHTML += `${c} \u{1F449}`
+                }
+            } else {
+                //contagem decrescente
+                for(let c = i; c <= f; c += p) {
+                    res.innerHTML += `${c} \u{1F449}`
+                }
+            }
+            res.innerHTML += `\u{1F3C1}`
+        }
     }
     
-}
+
+*/
+
+// &#x1F449
+// &#x1F3C1
--- exercicios/atividade04/script.js
+++ exercicios/atividade04/script.js
@@ -1,1 +1,46 @@
-// alert('Olá!')
+function tabuada() {
+    let num = document.getElementById('num')
+    let tab = document.getElementById('seltab')
+    let tabSoma = document.getElementById('selsomar')
+    let tabDiv = document.getElementById('seldiv')
+    let tabDim = document.getElementById('seldim')
+    if(num.value.length == 0)
+    {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while(c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+        c=1
+        tabSoma.innerHTML = ''
+        while(c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} + ${c} = ${n+c}`
+            tabSoma.appendChild(item)
+            c++
+        }
+        c = 1
+        tabDiv.innerHTML = ''
+        while(c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} / ${c} = ${(n/c).toFixed(2)}`
+            tabDiv.appendChild(item)
+            c++
+        }
+        c = 1
+        tabDim.innerHTML = ''
+        while(c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} - ${c} = ${n-c}`
+            tabDim.appendChild(item)
+            c++
+        }
+    }    
+}
