2b055d66e0e09ed1bcb3890cf5ba343effeb6f5e
dailtonccomp <112089713+dailtonccomp@users.noreply.github.com>
2022-10-03 10:58:44 -0300

    ex media ok

--- exercicios/atividade05/script.js
+++ exercicios/atividade05/script.js
@@ -1,1 +1,18 @@
-// alert('Olá!')
+var qtde_num = 0
+var soma = 0
+
+function somar() {    
+    let num = document.getElementById('inum')
+    let res = document.getElementById('res')
+    if(num.value.length == 0) {
+        window.alert('Por favor, informe um número')
+    } else {
+        let n = Number(num.value)
+        soma = soma + n
+        qtde_num++
+        let media = parseFloat((soma/qtde_num).toFixed(2))
+        res.innerHTML = `Média é: ${media}`
+    }
+    num.value = ''
+    res.innerHTML += `<p>Soma: ${soma}, Quantidade de numero ${qtde_num}</p>`
+}
