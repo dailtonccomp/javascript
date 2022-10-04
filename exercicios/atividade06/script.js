599db975f5a49c485610191456ed922f985d8c55
dailtonccomp <112089713+dailtonccomp@users.noreply.github.com>
2022-10-04 14:52:00 -0300

    exe06 ok

--- exercicios/atividade06/script.js
+++ exercicios/atividade06/script.js
@@ -1,19 +1,28 @@
 const n = []
-let i = 0
-function guardar() {
+let v = 0
+function guardar(i) {
     let num = document.getElementById('inum')
     if(num.value.length == 0) {
         window.alert('Por favor, informe um número')
     } else {        
-        for(i = 0; i < 4; i++) {
-            n[i] = nNumber(num.value)
-        }
-        res.innerHTML = `numeros é: ${n}`
+        // for(i = 0; i < 5; i++) {
+            n[i] = Number(num.value)
+        // }
+        num.value = ''
     }    
 }
 
+function incremento() {
+    return v++
+}
+
+function limpar() {
+    v = 0
+    res.innerHTML = ''
+}
+
 function quadrado() {
-    for(i = 0; i < 4; i++) {
-        res.innerHTML = `numeros é: ${n}`
+    for(i = 0; i < 5; i++) {
+        res.innerHTML += `Quadrado de ${n[i]} é: ${n[i]*n[i]} <br>`
     }
 }
