ac60fab1f3c700b478248e862968d7f7cd79acaa
dailtonccomp <112089713+dailtonccomp@users.noreply.github.com>
2022-10-10 17:53:48 -0300

    salvando!

--- node/mysql/app.js
+++ node/mysql/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 
+//iniciando o banco de dados
+const db = require('./models/db');
+
+//mensagem de teste
 app.get("/", async (req, res) => {
-    res.send("Página inicial");
+    res.send("Página inicial modificado.");
 });
+
+
 
 app.listen(8080, () => {
     console.log("Servidor iniciado na porta 8080://localhost:8080");
